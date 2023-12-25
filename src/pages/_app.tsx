fd25cd6ff3d71dd61cb0a550d184a77d28e11c57
Armaan Gupta <armaan@guppy.im>
2023-12-25 13:11:22 -0600

    init: fonts, next-seo

---
+++ src/pages/_app.tsx
@@ -0,0 +1,210 @@
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import CustomFont from "next/font/local";
+import { useRouter } from "next/router";
+import { DefaultSeo } from "next-seo";
+
+const satoshi = CustomFont({
+  src: [
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Regular.woff",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Regular.ttf",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Regular.eot",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Italic.woff",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Italic.woff2",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Italic.ttf",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Italic.eot",
+      weight: "400",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Medium.woff",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Medium.ttf",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Medium.eot",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-MediumItalic.woff",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-MediumItalic.woff2",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-MediumItalic.ttf",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-MediumItalic.eot",
+      weight: "500",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Bold.woff",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Bold.ttf",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-Bold.eot",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-BoldItalic.woff",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-BoldItalic.woff2",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-BoldItalic.ttf",
+      weight: "700",
+      style: "italic",
+    },
+    {
+      path: "../../public/fonts/Satoshi/Satoshi-BoldItalic.eot",
+      weight: "700",
+      style: "italic",
+    },
+  ],
+  variable: "--font-satoshi",
+});
+
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const canonicalUrl = (
+    `https://kreativedocuvet.com` + (router.asPath === "/" ? "" : router.asPath)
+  ).split("?")[0];
+
+  return (
+    <div className={`${satoshi.variable}`}>
+      <DefaultSeo
+        title={undefined}
+        titleTemplate="%s | Kreative DocuVet"
+        defaultTitle="Kreative DocuVet"
+        description="Kreative DocuVet is helping veterinarians get home on time with our AI scribe that turns raw appointment audio into completed medical records in minutes."
+        canonical={canonicalUrl}
+        additionalLinkTags={[
+          {
+            rel: "icon",
+            href: "/favicon.ico",
+          },
+          {
+            rel: "apple-touch-icon",
+            href: "/apple-touch-icon.png",
+            sizes: "180x180",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-32x32.png",
+            sizes: "32x32",
+          },
+          {
+            rel: "icon",
+            href: "/favicon-16x16.png",
+            sizes: "16x16",
+          },
+          {
+            rel: "manifest",
+            href: "/site.webmanifest",
+          },
+          {
+            rel: "mask-icon",
+            href: "/safari-pinned-tab.svg",
+            color: "#5bbad5",
+          },
+        ]}
+        additionalMetaTags={[
+          {
+            name: "ms-application-TileColor",
+            content: "#da532c",
+          },
+          {
+            name: "theme-color",
+            content: "#ffffff",
+          },
+        ]}
+        openGraph={{
+          url: "https://kreativedocuvet.com/",
+          title: "Kreative DocuVet",
+          description:
+            "Kreative DocuVet is helping veterinarians get home on time with our AI scribe that turns raw appointment audio into completed medical records in minutes.",
+          images: [
+            {
+              url: "/images/docuvet-profile-cover-sqr.png",
+              width: 700,
+              height: 700,
+              alt: "Kreative DocuVet Square Logo Wordmark",
+            },
+          ],
+        }}
+        twitter={{
+          handle: "@kreativedocuvet",
+          site: "@kreativedocuvet",
+          cardType: "summary_large_image",
+        }}
+      />
+      <Component {...pageProps} />
+    </div>
+  );
+}

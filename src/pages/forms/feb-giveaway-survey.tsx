a85a2a6e76123b4c1e385e747140da8f47ae2039
Armaan Gupta <armaan@guppy.im>
2024-01-25 22:00:08 -0600

    feat: added giveaway typeform page

---
+++ src/pages/forms/feb-giveaway-survey.tsx
@@ -0,0 +1,38 @@
+import Container from "../../components/Container";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
+import { NextSeo } from "next-seo";
+import { Widget } from "@typeform/embed-react";
+
+export default function FebGiveawaySurvey() {
+  return (
+    <div>
+      <NextSeo
+        title="FREE LUNCH for your clinic! | February Giveaway"
+        description="Enter to win FREE LUNCH for your clinic! We promise it'll only take 2 minutes and nothing more."
+      />
+      <Navbar />
+      <Container>
+        <div className="text-center pt-4 md:pt-8 pb-12">
+          <h1 className="text-3xl md:text-4xl font-bold tracking-tight">
+            Enter to win FREE LUNCH for your clinic! 
+          </h1>
+          <p className="text-lg md:text-xl text-neutrals-9 pt-3 pb-6">
+            We promise it&apos;ll only take{" "}
+            <span className="font-bold">2 minutes</span> and nothing more.
+          </p>
+          <div className="bg-gradient-to-r from-brand-forrest to-brand-deepocean p-1.5 rounded-[12px]">
+            <div className="bg-white rounded-[10px]">
+              <Widget
+                id={"ozU35bax"}
+                style={{ width: "100%", height: "60vh" }}
+                inlineOnMobile={true}
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+      <Footer />
+    </div>
+  );
+}
